d5b81df7340e8a6521a227704d381110e4036de6
Soap404 <alarjawia@gmail.com>
2025-11-20 12:29:35 +0100

    UPDATE NAVBAR LINKS IN HEADER
    - Changed "Navbar" text to "ONLINE SHOP".
    - Updated "Home" link to navigate to `index.php`.

--- Templates/header.php
+++ Templates/header.php
@@ -21,7 +21,7 @@ if (isset($_POST['logout'])) {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary px-5">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="index.php">ONLINE SHOP</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -29,7 +29,7 @@ if (isset($_POST['logout'])) {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="register.php">Register</a>
